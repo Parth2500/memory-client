20698a82857a08d07cc6ca056a515f17b368004e
Parth2500 <pmakawana257@gmail.com>
2021-11-16 22:26:20 +0530

    gg

---
+++ precache-manifest.0001db55101fccdba8cf8202e1ca5a1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9f819586892fe1a4433e81371c28fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9f05ac0472245254e38",
+    "url": "/static/css/main.056dd9fc.chunk.css"
+  },
+  {
+    "revision": "f48118e886219a32f479",
+    "url": "/static/js/2.22a44bb8.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.22a44bb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9f05ac0472245254e38",
+    "url": "/static/js/main.6cdf500b.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
